45413d502dea96b77136686ae093894237c40ae7
Jauhien Lipko <nowmdr@protonmail.com>
2025-09-18 15:22:05 +0200

    gh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // Добавьте basePath если ваш репозиторий не является GitHub Pages по умолчанию
+  // basePath: '/ninethree.studio'
 };
 
 export default nextConfig;
